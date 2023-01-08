775428525f795ea51af06cc73d997eebbdee31b1
mehrab-haque <mehrab.haque.0001@gmail.com>
2023-01-08 13:44:39 +0600

    multiple solution bug fixed

--- Offline 2/strategy.js
+++ Offline 2/strategy.js
@@ -1,46 +1,45 @@
 
 
 const backTrack=(board,heuristic)=>{
-
-    //dev
-    //return
-
     var currCell=heuristic(board.state)
     if(currCell===null){
         board.state.map(r=>{
-            var row=[]
+            var row=''
             r.map(c=>{
-                row.push(c.value)
+                row+=`${c.value}\t`
             })
             console.log(row)
         })
         console.log('solved')
+        return true
     }else{
         var sortedDomain=getLeastConstariningValues(board,currCell)
-        sortedDomain.map(domainValue=>{
+        for(var i in sortedDomain){
+            var domainValue=sortedDomain[i]
             if(isConsistent(board,currCell,domainValue)) {
                 currCell.assignValue(domainValue)
-                backTrack(board, heuristic)
+                if(backTrack(board, heuristic))
+                    return true
                 currCell.unassignValue(domainValue)
             }
-        })
+        }
+        return false
     }
 }
 
 
 const getLeastConstariningValues=(board,cell)=>{
-    //need to write logic here
     var sortedDomainList=JSON.parse(JSON.stringify(cell.domain))
     sortedDomainList.sort(function(a,b){
         var aCount=0,bCount=0
         for(var x=0;x<board.n;x++){
             if(x!==cell.col){
                 var rowCell=board.state[x][cell.col]
-                if(rowCell.value===a)aCount++
+                if(rowCell.domain.indexOf(a))aCount++
             }
             if(x!==cell.row){
                 var colCell=board.state[cell.row][x]
-                if(colCell.value===b)bCount++
+                if(colCell.domain.indexOf(b))bCount++
             }
         }
         return aCount-bCount
